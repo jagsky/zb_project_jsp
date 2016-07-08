94ac724c67a00b9cc7778f31395a8bf345543137
Administrator <www.275139314@qq.com>
2016-07-08 15:43:18 +0800

    重新上传

--- src/com/zb/dao/UserDao.java
+++ src/com/zb/dao/UserDao.java
@@ -1,8 +1,24 @@
 package com.zb.dao;
 
+import com.zb.sql.SqlDataUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by Administrator on 2016/7/8.
  */
 public class UserDao {
     //查询身份证是否存在
+    public boolean queryIdCard(String idCard) throws SQLException {
+        Connection conn = SqlDataUtil.getConnection();
+        String sql = "";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, idCard);
+        ResultSet rs = ps.executeQuery();
+        boolean s = rs.next();
+        return s;
+    }
 }
--- src/com/zb/servlet/RegisterServlet.java
+++ src/com/zb/servlet/RegisterServlet.java
@@ -16,6 +16,7 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/xml");
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
